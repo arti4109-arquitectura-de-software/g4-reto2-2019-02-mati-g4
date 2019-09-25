2fe909b385c781c829fc8599e8f1102b67b7fa79
pepito <isoka20@hotmail.com>
2019-09-24 23:06:47 -0500

    crear proyecto

---
+++ Controllers/InvoiceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Challenger2.Hubs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Challenger2.Controllers
+{
+    public class Invoice
+    {
+
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        private IHubContext<ValidateHub, ITypedHubClient> _hubContext;
+
+        public InvoiceController(IHubContext<ValidateHub, ITypedHubClient> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public bool InvoiceProcess(Invoice invoice)
+        {
+
+            _hubContext.Clients.All.BroadcastMessage("error", "validación fallida");
+
+            return true;
+        }
+    }
+}
---
+++ Hubs/ValidateHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenger2.Hubs
+{
+    public interface ITypedHubClient
+    {
+        Task BroadcastMessage(string type, string payload);
+    }
+
+    public class ValidateHub : Hub<ITypedHubClient>
+    {
+        
+    }
+}

d068506f3bd93abc6cdc1a5fff0bd4d03e42e919
pepito <isoka20@hotmail.com>
2019-09-26 13:57:36 -0500

    Validar internet

--- Challenger2/ClientApp/src/app/app.component.ts
+++ Challenger2/ClientApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription, fromEvent } from 'rxjs';
 import * as signalR from "@aspnet/signalr";
 
 @Component({
@@ -7,8 +7,18 @@ import * as signalR from "@aspnet/signalr";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'challenger2';
+
+  onlineEvent: Observable<Event>;
+  offlineEvent: Observable<Event>;
+
+  subscriptions: Subscription[] = [];
+
+  connectionStatusMessage: string;
+  connectionStatus: string;
+
+
 
   private hubConnection: signalR.HubConnection;
 
@@ -17,9 +27,34 @@ export class AppComponent {
   constructor() {
     this.GetMessage();
     this.hubConnection.on('BroadcastMessage', (type: string, payload: string) => {
+      alert("Tipo : " + type +"\n Mensaje :" + payload);
       console.log(type);
       console.log(payload);
     });
+
+    setInterval(function () {
+
+      var request = new XMLHttpRequest()
+
+      request.open('GET', 'https://google.com', true)
+      request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response)
+
+        if (request.status >= 200 && request.status < 400) {
+          
+        } else {
+          console.log('error')
+        }
+      }
+
+      request.send()
+
+    }, 3000);
+
+    window.onoffline = function () {
+      alert("No hay internet");
+    }
   }
 
   GetMessage() {
@@ -44,4 +79,37 @@ export class AppComponent {
       func();
     });
   }
+
+  ngOnInit(){
+
+    /**
+    * Get the online/offline status from browser window
+    */
+    this.onlineEvent = fromEvent(window, 'online');
+    this.offlineEvent = fromEvent(window, 'offline');
+
+    this.subscriptions.push(this.onlineEvent.subscribe(e => {
+      this.connectionStatusMessage = 'Back to online';
+      this.connectionStatus = 'online';
+      console.log('Online...');
+      alert("Online");
+    }));
+
+    this.subscriptions.push(this.offlineEvent.subscribe(e => {
+      this.connectionStatusMessage = 'Connection lost! You are not connected to internet';
+      this.connectionStatus = 'offline';
+      console.log('Offline...');
+      alert("Offline");
+    }));
+  }
+
+  ngOnDestroy(){
+    /**
+    * Unsubscribe all subscriptions to avoid memory leak
+    */
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
 }
+
+
+
--- Challenger2/Controllers/InvoiceController.cs
+++ Challenger2/Controllers/InvoiceController.cs
@@ -11,8 +11,31 @@ namespace Challenger2.Controllers
 {
     public class Invoice
     {
+        public string TerminalCode { get; set; }
+        public string Date { get; set; }
 
+        public string description { get; set; }
+
+        public string NroInvoice { get; set; }
+
+        public string SaleCode { get; set; }
+        public string Value { get; set; }
+
+        public ValidType response { get; set; }
     }
+
+    public enum ValidType
+    {
+        IpNoValid,
+        HashingNoFound,
+        RowNotValid,
+        NoNumberFacture,
+        ProductValueFiled,
+        FailAutentication,
+        NumberNoFoundPos,
+        FailedPos
+    }
+
 
     [Route("api/[controller]")]
     [ApiController]
@@ -28,6 +51,36 @@ namespace Challenger2.Controllers
         [HttpPost]
         public bool InvoiceProcess(Invoice invoice)
         {
+
+            string message = string.Empty;
+
+            switch(invoice.response)
+            {
+                case ValidType.FailAutentication:
+                    message = "falla en autenticación";
+                    break;
+                case ValidType.FailedPos:
+                    message = "El pos a enviado mas de 10 veces datos de facturación enviada";
+                    break;
+                case ValidType.HashingNoFound:
+                    message = "No es psoible autenticar el POS";
+                    break;
+                case ValidType.IpNoValid:
+                    message = "La dirección ip no corresponde a los POS";
+                    break;
+                case ValidType.NoNumberFacture:
+                    message = "No existe ese numero de factura";
+                    break;
+                case ValidType.NumberNoFoundPos:
+                    message = "Ese numero de factuación no se ha asignado al pos";
+                    break;
+                case ValidType.ProductValueFiled:
+                    message = "Costo del producto no valido";
+                    break;
+                case ValidType.RowNotValid:
+                    message = "Campos no validos";
+                    break;
+            }
 
             _hubContext.Clients.All.BroadcastMessage("error", "validación fallida");
 
